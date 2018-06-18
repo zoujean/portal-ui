2188b6b0f3df7947d5ad529046f77eaf4067fa04
Ann Catton <anncatton@gmail.com>
2018-06-18 13:45:02 -0400

    try with just creds include

--- src/packages/@ncigdc/utils/ajax/index.js
+++ src/packages/@ncigdc/utils/ajax/index.js
@@ -7,24 +7,25 @@ import md5 from 'blueimp-md5';
 
 const DEFAULTS = {
   method: 'get',
-  credentials: 'same-origin',
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': true,
-    'X-Auth-Token': 'secret admin token',
-  },
+  credentials: 'include',
+  // credentials: 'same-origin',
+  // headers: {
+  //   'Content-Type': 'application/json',
+  //   'Access-Control-Allow-Origin': true,
+  //   'X-Auth-Token': 'secret admin token',
+  // },
 };
 
 export function fetchAuth(options: { endpoint: string }): Object {
   const apiCall = {
     [CALL_API]: {
       ...DEFAULTS,
-      ...(IS_AUTH_PORTAL !== 'false'
-        ? {
-            credentials: 'include',
-            headers: { 'Content-Type': 'application/json' },
-          }
-        : {}),
+      // ...(IS_AUTH_PORTAL !== 'false'
+      //   ? {
+      //       credentials: 'include',
+      //       headers: {},
+      //     }
+      //   : {}),
       ...options,
       endpoint: urlJoin(AUTH, options.endpoint),
     },
