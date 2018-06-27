de99bebe2d8c5a47022e0f536ee3e45e824301ec
Ann Catton <anncatton@gmail.com>
2018-06-27 12:21:28 -0400

    add zoid component to window

--- src/packages/@ncigdc/components/LoginButton.js
+++ src/packages/@ncigdc/components/LoginButton.js
@@ -8,7 +8,7 @@ import { fetchUser } from '@ncigdc/dux/auth';
 import LocationSubscriber from '@ncigdc/components/LocationSubscriber';
 import styled from '@ncigdc/theme/styled';
 import { AUTH, FENCE } from '@ncigdc/utils/constants';
-
+import zoid from 'zoid';
 /*----------------------------------------------------------------------------*/
 
 let first = true;
@@ -24,7 +24,11 @@ const openAuthWindow = ({
 }) => {
   if (navigator.cookieEnabled) {
     const win = open(winUrl, 'Auth', winStyle);
-    window.loginPopup = win;
+    const zoidLogin = zoid.create({
+      tag: 'zoid-login',
+      url: winUrl,
+    });
+    win.zoidPopup = zoidLogin;
     console.log('is first, top? ', first, 'win: ', win);
     const interval = setInterval(() => {
       console.log('setting interval');
