45995322a41657c8ea4024d5dd95720e16139977
Sam Rich <samrichca@users.noreply.github.com>
2019-05-07 13:01:10 -0400

    eslint changes

--- src/packages/@ncigdc/modern_components/ProjectsTable/ProjectsTable.js
+++ src/packages/@ncigdc/modern_components/ProjectsTable/ProjectsTable.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import { compose, setDisplayName, mapProps } from 'recompose';
 import { Row } from '@ncigdc/uikit/Flex';
 import TableActions from '@ncigdc/components/TableActions';
-import tableModels from '@ncigdc/tableModels';
 import timestamp from '@ncigdc/utils/timestamp';
 
 import Table, { Tr, Td } from '@ncigdc/uikit/Table';
@@ -21,7 +20,6 @@ export default compose(
   ({
     downloadable,
     hits,
-    params,
     entityType = 'projects',
     tableHeader,
     tableColumns,
@@ -39,36 +37,31 @@ export default compose(
         >
           {tableHeader && <h3 className="panel-title">{tableHeader}</h3>}
           <div>
-            <b>{hits.total}</b> Projects
+            <b>{hits.total}</b>
+            {' '}
+            Projects
           </div>
           <TableActions
-            type="project"
-            scope="repository"
             arrangeColumnKey={entityType}
-            total={hits.total}
-            endpoint={entityType}
             downloadable={downloadable}
             downloadFields={tableInfo
               .filter(x => x.downloadable)
               .map(x => x.field || x.id)}
+            endpoint={entityType}
+            scope="repository"
             sortOptions={tableInfo.filter(x => x.sortable)}
-            tsvSelector="#projects-table"
+            total={hits.total}
             tsvFilename={`projects-table.${timestamp()}.tsv`}
+            tsvSelector="#projects-table"
+            type="project"
           />
         </Row>
         <div style={{ overflowX: 'auto' }}>
           <Table
-            id="projects-table"
-            headings={tableInfo
-              .filter(x => !x.subHeading)
-              .map(x => <x.th key={x.id} />)}
-            subheadings={tableInfo
-              .filter(x => x.subHeading)
-              .map(x => <x.th key={x.id} />)}
-            body={
+            body={(
               <tbody>
                 {hits.edges.map((e, i) => (
-                  <Tr key={e.node.id} index={i}>
+                  <Tr index={i} key={e.node.id}>
                     {tableInfo
                       .filter(x => x.td)
                       .map(x => <x.td key={x.id} node={e.node} />)}
@@ -79,16 +72,22 @@ export default compose(
                     tableInfo
                       .filter(x => x.td)
                       .map(
-                        x =>
-                          x.total ? (
-                            <x.total key={x.id} hits={hits} />
-                          ) : (
+                        x => (x.total ? (
+                          <x.total hits={hits} key={x.id} />
+                        ) : (
                             <Td key={x.id} />
-                          )
+                          ))
                       )}
                 </Tr>
               </tbody>
-            }
+            )}
+            headings={tableInfo
+              .filter(x => !x.subHeading)
+              .map(x => <x.th key={x.id} />)}
+            id="projects-table"
+            subheadings={tableInfo
+              .filter(x => x.subHeading)
+              .map(x => <x.th key={x.id} />)}
           />
         </div>
       </div>
